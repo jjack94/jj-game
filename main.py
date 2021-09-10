dc845fc023bfd513c46ed621637c58959a47e407
jjack94 <77504348+jjack94@users.noreply.github.com>
2021-09-10 14:15:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import jjgame
 import globalvaribles
 
-# begin game 
+# launch from here to begin game 
 def main():
     globalvaribles.gVariables()
     while globalvaribles.playername != "Name":
